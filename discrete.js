61ac4065bd26760138dccd3ce3b04e9d82f10a86
james-lepore <43451533+james-lepore@users.noreply.github.com>
2021-04-13 22:09:40 -0400

    Cooking theme applied
    And style/functional changes

--- discrete.js
+++ discrete.js
@@ -1,7 +1,7 @@
 function reset_borders(){
     let boxes = [document.getElementById("A"), document.getElementById("B"), document.getElementById("C"), document.getElementById("D"), document.getElementById("E"), document.getElementById("F")];
     for(box of boxes){
-        box.style.borderColor = "#666";
+        box.style.backgroundColor = "white";
     }
 }
 
@@ -9,29 +9,31 @@ function submit(){
   let result = 1;
   let boxes = [document.getElementById("A"), document.getElementById("B"), document.getElementById("C"), document.getElementById("D"), document.getElementById("E"), document.getElementById("F")];
   let answerKey = [["There are 7 continents.","10 is a prime number.","Monday will be cloudy."],["What time is it?","Shut the door.","Have a nice day."]];
-  let mesg = document.getElementById("message");
 
   for(let i = 0; i < boxes.length; i++){    
-      if(((answerKey[0].includes(boxes[i].innerHTML)) && (i < 3)) || ((answerKey[1].includes(boxes[i].innerHTML)) && (i > 2))){
+      if(((answerKey[0].includes(boxes[i].lastChild.innerHTML)) && (i < 3)) || ((answerKey[1].includes(boxes[i].lastChild.innerHTML)) && (i > 2))){
           result*=1;
-          boxes[i].style.borderColor = "green";
+          boxes[i].style.backgroundColor = "lightgreen";
       } else {
           result*=0;
-          boxes[i].style.borderColor = "red";
+          boxes[i].style.backgroundColor = "lightsalmon";
       }
   }
  
   var modal = document.getElementById("myModal");
   var modal_text = document.getElementById("modal-text");
+  var italian = document.getElementById("italian");
   if(result){
       var nxts = document.getElementsByClassName('next_arrow');
       for(i = 0; i < nxts.length; i++) {
           nxts[i].style.visibility = 'visible';
       }
       modal_text.innerHTML = "Well done!";
+      italian.src = "images/chef_goodjob.png";
       modal.style.display = "block";
   } else{
-      modal_text.innerHTML = "Try again!";
+      modal_text.innerHTML = "Sorry, try again!";
+      italian.src = "images/chef_sad.png";
       modal.style.display = "block";
   }
 }
@@ -50,9 +52,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     var dragSrcEl = null;
     
-    function handleDragStart(e) {
-      this.style.opacity = '0.4';
-     
+    function handleDragStart(e) { 
       dragSrcEl = this;
   
       e.dataTransfer.effectAllowed = 'move';
@@ -91,18 +91,14 @@ document.addEventListener('DOMContentLoaded', (event) => {
         for(i = 0; i < nxts.length; i++) {
             nxts[i].style.visibility = 'hidden';
         }
-        if(this.innerHTML == ""){
-            this.style.backgroundColor = "white";
+        if(this.lastChild.innerHTML == ""){
             this.setAttribute("draggable", "false");
         } else {
-            this.style.backgroundColor = "#ddd";
             this.setAttribute("draggable", "true");
         }
-        if(dragSrcEl.innerHTML == ""){
-            dragSrcEl.style.backgroundColor = "white";
+        if(dragSrcEl.lastChild.innerHTML == ""){
             dragSrcEl.setAttribute("draggable", "false");
         } else {
-            dragSrcEl.style.backgroundColor = "#ddd";
             dragSrcEl.setAttribute("draggable", "true");
         }
         
