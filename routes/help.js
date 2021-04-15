6f12bc30d6be967c260e12eee52686418fe37d99
mattdim2010 <32401212+mattdim2010@users.noreply.github.com>
2021-04-15 19:22:02 -0400

    Set up main code on master branch - routes folder

--- routes/help.js
+++ routes/help.js
@@ -4,10 +4,10 @@ const router = express.Router();
 router.get('/', async (req, res) => {
   if (req.session.user) {
     req.session.user.log = true;
-    res.render('pages/about', { loggedin: true, currentUser: req.session.user, userID: req.session.uid});
+    res.render('pages/help', { loggedin: true, currentUser: req.session.user, userID: req.session.uid});
   }
   else {
-    res.render('pages/about');
+    res.render('pages/help');
   }
 });
 
