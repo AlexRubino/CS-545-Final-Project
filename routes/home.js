63287db46499a8643df04a46c8d7c27f7860bb86
james-lepore <43451533+james-lepore@users.noreply.github.com>
2021-05-03 18:44:33 -0400

    Minor Changes
    Fix up style
    Add visual component of tutorial
    Finish discrete lesson
    Add progression system for HCI

--- routes/home.js
+++ routes/home.js
@@ -6,7 +6,7 @@ router.get('/', async (req, res) => {
   if (req.session.user) {
     req.session.user.log = true;
     const currentUserData = await userData.getUserById(req.session.uid);
-    res.render('pages/index', { loggedin: true, currentUser: req.session.user, userID: req.session.uid, discreteProgress: currentUserData.discreteLessonProgress});
+    res.render('pages/index', { loggedin: true, currentUser: req.session.user, userID: req.session.uid, discreteProgress: currentUserData.discreteLessonProgress, hciProgress: currentUserData.hciLessonProgress});
   }
   else {
     res.render('pages/index');
