e7947912f56158a21f546a58344bdea35c87f0d6
mattdim2010 <32401212+mattdim2010@users.noreply.github.com>
2021-05-03 22:45:12 -0400

    Added tutorial increment database functionality

--- routes/tutorial.js
+++ routes/tutorial.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const userData = require('../data/users');
 
 router.get('/', async (req, res) => {
   if (req.session.user) {
     req.session.user.log = true;
-    res.render('pages/tutorial', { loggedin: true, currentUser: req.session.user, userID: req.session.uid});
+    const currentUserData = await userData.getUserById(req.session.uid);
+    console.log(currentUserData);
+    console.log(currentUserData.tutorialCount);
+    const lesson1Completed = await userData.updateTutorialCount(req.session.uid, currentUserData.tutorialCount + 1);
+    console.log(currentUserData.tutorialCount);
+    res.render('pages/tutorial', { loggedin: true, currentUser: req.session.user, userID: req.session.uid, tutorialCount: currentUserData.tutorialCount});
   }
   else {
     res.render('pages/tutorial');
